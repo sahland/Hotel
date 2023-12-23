2d11b0cc41bc06ef5bf01faff6d429ce0600c3f4
sahland <sahland@mail.ru>
2023-12-23 22:09:21 +0300

    Connected routes

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 import uvicorn
 
-from core.config import settings
 from fastapi import FastAPI
+from fastapi_pagination import add_pagination
 from contextlib import asynccontextmanager
 
+from core.config import settings
+from api_v1 import router as router_v1
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -11,6 +13,10 @@ async def lifespan(app: FastAPI):
 
 def create_app():
     app = FastAPI(lifespan=lifespan,title='Hotel REST API')
+    
+    add_pagination(app)
+
+    app.include_router(router=router_v1, prefix=settings.api_v1_prefix)
     return app
 
 if __name__ == '__main__':
