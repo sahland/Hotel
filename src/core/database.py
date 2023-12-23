5ea2c9de6e767d5c41810565a1bff1b473472208
sahland <sahland@mail.ru>
2023-12-23 18:54:03 +0300

    Create database module with Database class for connection

---
+++ src/core/database.py
@@ -0,0 +1,40 @@
+from asyncio import current_task
+from sqlalchemy.ext.asyncio import (
+    create_async_engine, 
+    async_sessionmaker, 
+    async_scoped_session,
+    AsyncSession
+)
+
+from .config import settings
+
+class Database:
+
+    def __init__(self, url: str, echo: bool = False) -> None:
+        
+        self.engine = create_async_engine(
+            url=url,
+            echo=echo)
+        
+        self.session_factory = async_sessionmaker(
+            bind=self.engine,
+            autoflush=False,
+            autocommit=False,
+            expire_on_commit=False
+        )
+    
+    def get_scoped_session(self):
+        session = async_scoped_session(
+            session_factory=self.session_factory,
+            scopefunc=current_task,
+        )
+        return session
+
+    async def scoped_session_dependency(self) -> AsyncSession:
+        session = self.get_scoped_session()
+        yield session
+        await session.close()
+
+db_helper = Database(
+    url=settings.database_url_asyncpg
+)
