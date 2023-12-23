8d57262b8d9ae2e61bb2187ee6fcbb9a15d68097
sahland <sahland@mail.ru>
2023-12-23 22:24:17 +0300

    Created client model

--- src/core/model.py
+++ src/core/model.py
@@ -1,3 +1,4 @@
+import datetime
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped, mapped_column
 
@@ -10,3 +11,11 @@ class RoomTypeORM(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement='auto')
     room_type_name: Mapped[str] = mapped_column(unique=True)
+
+class ClientORM(Base):
+    __tablename__='client'
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement='auto')
+    client_fullName: Mapped[str]
+    client_adress: Mapped[str]
+    client_phone_number: Mapped[str]
+    client_bithday: Mapped[datetime.date]
